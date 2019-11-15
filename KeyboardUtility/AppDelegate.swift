1d8b6b239994105b4f4b16fbbd872eea307b9ecb
Gabor Vass <gabor@gramble.com>
2019-11-15 14:32:23 +0100

    Initial Commit

---
+++ KeyboardUtility/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  KeyboardUtility
+//
+//  Created by Gábor Vass on 15/11/2019.
+//  Copyright © 2019 Gábor Vass. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ KeyboardUtility/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KeyboardUtility
+//
+//  Created by Gábor Vass on 15/11/2019.
+//  Copyright © 2019 Gábor Vass. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
